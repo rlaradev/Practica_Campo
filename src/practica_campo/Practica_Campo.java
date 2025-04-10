0262323f6bdee040e1a020739961b9612b5ffaf1
Renzo Enrique Lara Condori <127690927+rlaradev@users.noreply.github.com>
2025-04-09 23:08:05 -0500

    Se modifica clase Main

--- src/practica_campo/Practica_Campo.java
+++ src/practica_campo/Practica_Campo.java
@@ -15,6 +15,13 @@ public class Practica_Campo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Producto producto = new Producto();
+        
+        producto.agregar_producto(01, "Laptop", "Tecnología", 4150, 40);
+        producto.agregar_producto(01, "Iphone", "Tecnología", 2500, 75);
+        producto.agregar_producto(01, "Tablet", "Tecnología", 840, 200);
+        
+        producto.mostrar_producto();
     }
     
 }
